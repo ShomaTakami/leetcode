8c46d4f23d0b7b308295111a1a713f89802cb9c2
ShomaTakami <hawklook.2331@gmail.com>
2026-01-27 22:56:12 +0900

    [125] Valid Palindrome

---
+++ code/easy_125_valid_palindrome.go
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode id=125 lang=golang
+ *
+ * [125] Valid Palindrome
+ */
+
+// @lc code=start
+func isPalindrome(s string) bool {
+	left := 0
+	right := len(s) - 1
+	for left < right {
+		if !isAlphanumeric(s[left]) {
+			left++
+			continue
+		}
+		if !isAlphanumeric(s[right]) {
+			right--
+			continue
+		}
+
+		if unicode.ToLower(rune(s[left])) != unicode.ToLower(rune(s[right])) {
+			return false
+		}
+		left++
+		right--
+	}
+	return true
+}
+
+func isAlphanumeric(b byte) bool {
+	return (b >= 'a' && b <= 'z') ||
+		(b >= 'A' && b <= 'Z') ||
+		(b >= '0' && b <= '9')
+}
+
+// @lc code=end
